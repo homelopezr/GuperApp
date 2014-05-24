fa56d8148d6d121c82e107b611c20f64ed5bee7f
adrianlrx <adrianlrx@gmail.com>
2014-05-24 00:42:21 -0400

    FXUIo fix venta

--- src/jguperapp/tables/VentaView.java
+++ src/jguperapp/tables/VentaView.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package jguperapp.tables;
 
 import java.awt.EventQueue;
@@ -412,5 +406,5 @@ public class VentaView extends JPanel {
             }
         });
     }
-    
+        
 }
